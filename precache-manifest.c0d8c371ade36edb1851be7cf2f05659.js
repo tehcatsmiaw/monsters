4a067ca972f642573768e07afa79cd8def13a874
Andrei Locic <andrei.locic@gmail.com>
2020-05-29 10:55:01 +0100

    Updates

---
+++ precache-manifest.c0d8c371ade36edb1851be7cf2f05659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a6f6912f650f23c325c78614f55f6f",
+    "url": "/tehcatsmiaw/monsters/index.html"
+  },
+  {
+    "revision": "f50e1b3a0feaf6535e30",
+    "url": "/tehcatsmiaw/monsters/static/css/main.d0b05798.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/tehcatsmiaw/monsters/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tehcatsmiaw/monsters/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50e1b3a0feaf6535e30",
+    "url": "/tehcatsmiaw/monsters/static/js/main.9d64b3f8.chunk.js"
+  },
+  {
+    "revision": "d7a597b636af8bf33642",
+    "url": "/tehcatsmiaw/monsters/static/js/runtime-main.1c00748c.js"
+  }
+]);
